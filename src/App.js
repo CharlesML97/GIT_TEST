b855a89c42ce3b443599a89c893a75a2ede8d810
Paula Robles <paula.robles@solera.com>
2023-02-22 01:33:17 -0600

    3'

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload - this is the change.
-          Adding commit 2'
+          Adding commit 3'
         </p>
         <a
           className="App-link"
