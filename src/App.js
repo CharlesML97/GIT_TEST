de5d9e61279f7ea64f3bd6788714fd680d55550d
Martin Leon <cmartinleoncedillo@hotmail.com>
2023-02-22 01:32:09 -0600

    Commit 3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload - this is the change.
-          Adding commit2
+          Adding commit3
         </p>
         <a
           className="App-link"
